f8248eb46453cdad378bf85e935a997491e13e4d
Ogalababa <delay7.su@gmail.com>
2023-08-08 20:07:39 +0200

    mod controller

--- controller.py
+++ controller.py
@@ -7,31 +7,31 @@ OFF = GPIO.LOW
 
 
 def warm_fan(status):
-    GPIO.output(Params.WARM_FAN, status)
+    GPIO.output(params.WARM_FAN, status)
 
 
 def cool_fan(status):
-    GPIO.output(Params.COOL_FAN, status)
+    GPIO.output(params.COOL_FAN, status)
 
 
 def controller_fan(status):
-    GPIO.output(Params.COOL_FAN, status)
+    GPIO.output(params.COOL_FAN, status)
 
 
 def sun_lamp(status):
-    GPIO.output(Params.SUN_LAMP, status)
+    GPIO.output(params.SUN_LAMP, status)
 
 
 def night_lamp(status):
-    GPIO.output(Params.NIGHT_LAMP, status)
+    GPIO.output(params.NIGHT_LAMP, status)
 
 
 def uv_lamp(status):
-    GPIO.output(Params.UV_LAMP, status)
+    GPIO.output(params.UV_LAMP, status)
 
 
 def humidifier(status):
-    GPIO.output(Params.HUMIDIFIER, status)
+    GPIO.output(params.HUMIDIFIER, status)
 
 
 if __name__ == "__main__":
