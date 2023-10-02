de77f4009b58e930b3a060192ea952ff3eb4b607
Ogalababa <delay7.su@gmail.com>
2023-10-03 01:44:57 +0200

    更改字体 fit bug

--- oled_display.py
+++ oled_display.py
@@ -3,7 +3,7 @@ import json
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import sh1106
-from PIL import ImageFont, ImageDraw
+from PIL import ImageFont
 
 
 def read_status():
@@ -16,16 +16,14 @@ def display_on_oled():
     serial = i2c(port=1, address=0x3C)
     device = sh1106(serial, rotate=0)
 
-    font_path_small = "/usr/share/fonts/truetype/noto/NotoSansCJK-Regular.ttc"
-    font_path_large = "/usr/share/fonts/truetype/noto/NotoSansCJK-Regular.ttc"
+    font_path_small = "/usr/share/fonts/truetype/wqy/wqy-microhei.ttc"
+    font_path_large = "/usr/share/fonts/truetype/wqy/wqy-microhei.ttc"
 
     font_small = ImageFont.truetype(font_path_small, size=12)
     font_large = ImageFont.truetype(font_path_large, size=24)
 
     screen_width = device.width
     screen_height = device.height
-
-    dummy_draw = ImageDraw.Draw("RGB", (screen_width, screen_height))
 
     while True:
         data = read_status()  # Refresh the data every minute
@@ -34,7 +32,7 @@ def display_on_oled():
         while time.time() - start_time < 60:  # Keep displaying for 1 minute
             for key, value in data.items():
                 with canvas(device) as draw:
-                    value_width, value_height = dummy_draw.textsize(str(value), font=font_large)
+                    value_width, value_height = draw.textsize(str(value), font=font_large)
 
                     key_position = (0, 0)
                     value_position = (
