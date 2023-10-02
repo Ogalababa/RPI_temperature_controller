6ba296fa2e483034209a2fc9392ac157fe839028
Ogalababa <delay7.su@gmail.com>
2023-10-03 00:58:01 +0200

    show data in display

--- oled_display.py
+++ oled_display.py
@@ -4,10 +4,12 @@ from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import sh1106
 
+
 def read_status():
     with open("status.json", "r") as file:
         data = json.load(file)
     return data
+
 
 def display_on_oled(data):
     serial = i2c(port=1, address=0x3C)
@@ -20,11 +22,13 @@ def display_on_oled(data):
             draw.text((0, y), text, fill="white")
             y += 10  # Adjust depending on your font size
 
+
 def main():
     while True:
         data = read_status()
         display_on_oled(data)
         time.sleep(60)  # Wait for 1 minute before updating again
 
+
 if __name__ == "__main__":
-    main
+    main()
