3a5aa142f5ecac78e2c65c79692815debf4a4706
Ogalababa <delay7.su@gmail.com>
2023-10-03 01:47:46 +0200

    更改字体 fit bug

--- RTC.py
+++ RTC.py
@@ -68,8 +68,8 @@ class RTC:
                 hum_list.append(hum_1)
 
         if temp_list and hum_list:
-            self.temp = round(sum(temp_list) / len(temp_list), 2)
-            self.hum = round(sum(hum_list) / len(hum_list), 2)
+            self.temp = round(sum(temp_list) / len(temp_list), 1)
+            self.hum = round(sum(hum_list) / len(hum_list), 1)
 
     def get_control_temp(self):
         temp_list = []
@@ -88,8 +88,8 @@ class RTC:
 
         temp_final = sum(temp_list) / len(temp_list)
         hum_final = sum(hum_list) / len(temp_list)
-        self.control_temp = round(temp_final, 2)
-        self.control_hum = round(hum_final, 2)
+        self.control_temp = round(temp_final, 1)
+        self.control_hum = round(hum_final, 1)
 
     def controller(self, equipment, status):
 
