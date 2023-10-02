12710d2d0775ebb9d6482e99df7db4afd1e49992
Ogalababa <delay7.su@gmail.com>
2023-10-03 01:01:42 +0200

    show data in display

--- oled_display.py
+++ oled_display.py
@@ -3,6 +3,7 @@ import json
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import sh1106
+from PIL import ImageFont
 
 
 def read_status():
@@ -15,12 +16,15 @@ def display_on_oled(data):
     serial = i2c(port=1, address=0x3C)
     device = sh1106(serial, rotate=0)
 
+    font_path = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
+    font = ImageFont.truetype(font_path, size=12)
+
     with canvas(device) as draw:
         y = 0
         for key, value in data.items():
             text = f"{key}: {value}"
-            draw.text((0, y), text, fill="white")
-            y += 10  # Adjust depending on your font size
+            draw.text((0, y), text, fill="white", font=font)
+            y += 14  # Adjust depending on your font size
 
 
 def main():
