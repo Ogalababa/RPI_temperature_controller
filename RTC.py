14a5164df3c574d415bd708f029c36319f3a3f05
Ogalababa <delay7.su@gmail.com>
2023-10-02 15:01:20 +0200

    add TC 0.1

--- RTC.py
+++ RTC.py
@@ -1,149 +1,95 @@
+import time
 import json
 import RPi.GPIO as GPIO
-import time
 import Adafruit_DHT as DHT
 
 
 class RTC:
     # Class Constants
-    ON = GPIO.HIGH
-    OFF = GPIO.LOW
+    ON = "ON"
+    OFF = "OFF"
     NUM_RETRIES = 4
+
+    # Pin numbers
+    PINS = {
+        "TERMO_L": 26, "TERMO_M": 4, "TERMO_R": 17, "TERMO_F": 22, "TERMO_CL": 10,
+        "WARM_FAN": 11, "COOL_FAN": 5, "CONTROLLER_FAN": 13, "NIGHT_LAMP": 14,
+        "SUN_LAMP": 23, "UV_LAMP": 8, "HUMIDIFIER": 12
+    }
 
     # Initialization
     def __init__(self):
+
+        self.GPIO = GPIO
+        self.DHT = DHT
+        self.time = time
+
         # Set GPIO mode to use board nr 1-40
-        GPIO.setmode(GPIO.BCM)
+        self.GPIO.setmode(self.GPIO.BCM)
 
-        # Parameterizable
-        self.set_pin_numbers()
+        self.initialize_pins()
 
-        self.initialize_pins(self.pin_input, GPIO.IN)
-        self.initialize_pins(self.pin_output, GPIO.OUT)
-
-        # temp sensor
-        self.TEMP_SENSOR = DHT.DHT11
+        # Temp sensor
+        self.TEMP_SENSOR = self.DHT.DHT11
         self.temp = None
         self.hum = None
         self.control_temp = None
         self.control_hum = None
 
-        # status
-        self.init_status()
+        # Status
+        self.status = {key: None for key in self.PINS.keys()}
+        self.status["CONTROLLER_FAN"] = "N/A"
+        self.status["HUMIDIFIER"] = "N/A"
 
-        # initialization status set to off
-        for pin in self.pin_output:
+        # Initialization status set to off
+        for pin in self.PINS.values():
             self.controller(pin, self.OFF)
 
-    def set_pin_numbers(self):
-        self.TERMO_L = 26
-        self.TERMO_M = 4
-        self.TERMO_R = 17
-        self.TERMO_F = 22
-        self.TERMO_CL = 10
-        self.WARM_FAN = 11
-        self.COOL_FAN = 5
-        self.CONTROLLER_FAN = 13
-        self.NIGHT_LAMP = 14
-        self.SUN_LAMP = 23
-        self.UV_LAMP = 8
-        self.HUMIDIFIER = 12
-
-        self.pin_input = [self.TERMO_L, self.TERMO_M, self.TERMO_R, self.TERMO_F, self.TERMO_CL]
-        self.pin_output = [self.WARM_FAN, self.COOL_FAN, self.CONTROLLER_FAN, self.NIGHT_LAMP,
-                           self.SUN_LAMP, self.UV_LAMP, self.HUMIDIFIER]
-
-    def init_status(self):
-        self.warm_fan_status = None
-        self.cool_fan_status = None
-        self.controller_fan_status = "N/A"
-        self.night_lamp_status = None
-        self.sun_lamp_status = None
-        self.uv_lamp_status = None
-        self.humidifier_status = "N/A"
-
-    def initialize_pins(self, pins, mode):
-        for pin in pins:
-            GPIO.setup(pin, mode)
-            time.sleep(0.1)
+    def initialize_pins(self):
+        for pin in self.PINS.values():
+            self.GPIO.setup(pin,
+                            self.GPIO.OUT if "FAN" in pin or "LAMP" in pin or "HUMIDIFIER" in pin else self.GPIO.IN)
+            self.time.sleep(0.1)
 
     def get_room_temp(self):
         temp_list = []
         hum_list = []
-        termo_list = [self.TERMO_L, self.TERMO_R, self.TERMO_M, self.TERMO_F]
-
-        for i in termo_list:
-            temp_1, hum_1 = DHT.read_retry(self.TEMP_SENSOR, i)
-            if temp_1 is not None and hum_1 is not None:
-                temp_list.append(temp_1)
-                hum_list.append(hum_1)
-
-        if temp_list and hum_list:
-            temp = sum(temp_list) / len(temp_list)
-            hum = sum(hum_list) / len(hum_list)
-            self.temp = round(temp, 2)
-            self.hum = round(hum, 2)
-        else:
-            print("Failed to read temperature and humidity")
-
-    def get_control_temp(self):
-        temp_list = []
-        hum_list = []
-
-        for i in range(self.NUM_RETRIES):
-            temp, hum = DHT.read_retry(self.TEMP_SENSOR, self.TERMO_CL)
-            if temp is not None and hum is not None:
+        for _ in range(self.NUM_RETRIES):
+            hum, temp = self.DHT.read(self.TEMP_SENSOR, self.PINS["TERMO_L"])
+            if hum is not None and temp is not None:
                 temp_list.append(temp)
                 hum_list.append(hum)
             else:
-                time.sleep(5)
-            time.sleep(2)
+                self.time.sleep(5)
+            self.time.sleep(2)
 
         if temp_list and hum_list:
-            temp_final = sum(temp_list) / len(temp_list)
-            hum_final = sum(hum_list) / len(temp_list)
-            self.control_temp = round(temp_final, 2)
-            self.control_hum = round(hum_final, 2)
+            self.control_temp = round(sum(temp_list) / len(temp_list), 2)
+            self.control_hum = round(sum(hum_list) / len(temp_list), 2)
         else:
             print("Failed to read control temperature and humidity")
 
-    def controller(self, equipment, status, ):
-        GPIO.output(equipment, status)
-        set_dict = {self.ON: "ON", self.OFF: "OFF"}
-        status_str = set_dict.get(status)
-        if equipment == self.WARM_FAN:
-            self.warm_fan_status = status_str
-        elif equipment == self.COOL_FAN:
-            self.cool_fan_status = status_str
-        elif equipment == self.CONTROLLER_FAN:
-            self.controller_fan_status = status_str
-        elif equipment == self.NIGHT_LAMP:
-            self.night_lamp_status = status_str
-        elif equipment == self.SUN_LAMP:
-            self.sun_lamp_status = status_str
-        elif equipment == self.UV_LAMP:
-            self.uv_lamp_status = status_str
-        elif equipment == self.HUMIDIFIER:
-            self.humidifier_status = status_str
+    def controller(self, equipment, status):
+
+        if status is "ON":
+            setTo = GPIO.HIGH
+        else:
+            setTo = GPIO.LOW
+
+        self.GPIO.output(self.PINS[equipment], setTo)
+        self.status[equipment] = status
 
     def save_to_json(self):
-        data = {
+
+        data = self.status.copy()
+        data.update({
             'temp': self.temp,
             'hum': self.hum,
             'control_temp': self.control_temp,
             'control_hum': self.control_hum,
-            'warm_fan_status': self.warm_fan_status,
-            'cool_fan_status': self.cool_fan_status,
-            'controller_fan_status': self.controller_fan_status,
-            'night_lamp_status': self.night_lamp_status,
-            'sun_lamp_status': self.sun_lamp_status,
-            'uv_lamp_status': self.uv_lamp_status,
-            'humidifier_status': self.humidifier_status
-        }
+        })
         with open("status.json", "w") as json_file:
             json.dump(data, json_file, indent=4)
 
     def cleanup(self):
-        GPIO.cleanup()
-
-
+        self.GPIO.cleanup()
--- TemperatureController.py
+++ TemperatureController.py
@@ -1,51 +1,70 @@
+# Optimized TemperatureController.py
 import time
+from RTC import RTC
 from datetime import datetime
 import pytz
-from RTC import RTC
 
 class TemperatureController:
 
-    def __init__(self):
+    def __init__(self, target_temp=30, temp_range=5, timezone='Europe/Amsterdam'):
+
         self.rtc = RTC()
-        self.target_temp = 30
-        self.amsterdam = pytz.timezone('Europe/Amsterdam')
-        self.temp_range = 5
+        self.target_temp_day = target_temp
+        self.target_temp_night = target_temp - 3
+        self.temp_range = temp_range
+        self.timezone = pytz.timezone(timezone)
+        self.datetime = datetime
+        self.pytz = pytz
 
     def update_equipment_status(self, equipment, desired_status):
-        current_status = getattr(self.rtc, f'{equipment}_status')
+        current_status = self.rtc.status[equipment]
         if current_status != desired_status:
-            self.rtc.controller(getattr(self.rtc, equipment), getattr(self.rtc, desired_status))
+            self.rtc.controller(equipment, desired_status)
 
     def control_temperature(self):
         while True:
             self.rtc.get_room_temp()
             current_temp = self.rtc.temp
-            current_hour = datetime.now(self.amsterdam).hour
+            current_hour = self.datetime.now(self.timezone).hour
 
             if 10 <= current_hour < 24:
                 # check night lamp status:
-                self.update_equipment_status('night_lamp', 'OFF')
+                self.update_equipment_status('NIGHT_LAMP', self.rtc.OFF)
 
-                if current_temp < self.target_temp - self.temp_range:  # It's too cold
-                    self.update_equipment_status('sun_lamp', 'ON')
-                    self.update_equipment_status('warm_fan', 'ON')
-                    self.update_equipment_status('cool_fan', 'OFF')
+                if current_temp < self.target_temp_day - self.temp_range:  # It's too cold
+                    self.update_equipment_status('SUN_LAMP', self.rtc.ON)
+                    self.update_equipment_status('WARM_FAN', self.rtc.ON)
+                    self.update_equipment_status('COOL_FAN', self.rtc.OFF)
 
-                elif self.target_temp - 3 <= current_temp <= self.target_temp + 3:  # It's good temp
-                    self.update_equipment_status('sun_lamp', 'OFF')
-                    self.update_equipment_status('warm_fan', 'OFF')
-                    self.update_equipment_status('cool_fan', 'OFF')
+                elif self.target_temp_day - 3 <= current_temp <= self.target_temp_day + 3:  # It's good temp
+                    self.update_equipment_status('SUN_LAMP', self.rtc.OFF)
+                    self.update_equipment_status('WARM_FAN', self.rtc.OFF)
+                    self.update_equipment_status('COOL_FAN', self.rtc.OFF)
 
-                elif current_temp >= self.target_temp + self.temp_range:  # It's too hot
-                    self.update_equipment_status('sun_lamp', 'OFF')
-                    self.update_equipment_status('warm_fan', 'OFF')
-                    self.update_equipment_status('cool_fan', 'ON')
+                elif current_temp > self.target_temp_day + self.temp_range:  # It's too hot
+                    self.update_equipment_status('SUN_LAMP', self.rtc.OFF)
+                    self.update_equipment_status('WARM_FAN', self.rtc.OFF)
+                    self.update_equipment_status('COOL_FAN', self.rtc.ON)
 
             else:
-                self.update_equipment_status('sun_lamp', 'OFF')
+                self.update_equipment_status('SUN_LAMP', self.rtc.OFF)
 
+                if current_temp < self.target_temp_night - self.temp_range:  # It's too cold
+                    self.update_equipment_status('NIGHT_LAMP', self.rtc.ON)
+                    self.update_equipment_status('WARM_FAN', self.rtc.ON)
+                    self.update_equipment_status('COOL_FAN', self.rtc.OFF)
+
+                elif self.target_temp_night - 3 <= current_temp <= self.target_temp_night + 3:  # It's good temp
+                    self.update_equipment_status('NIGHT_LAMP', self.rtc.OFF)
+                    self.update_equipment_status('WARM_FAN', self.rtc.OFF)
+                    self.update_equipment_status('COOL_FAN', self.rtc.OFF)
+
+                elif current_temp > self.target_temp_night + self.temp_range:  # It's too hot
+                    self.update_equipment_status('NIGHT_LAMP', self.rtc.OFF)
+                    self.update_equipment_status('WARM_FAN', self.rtc.OFF)
+                    self.update_equipment_status('COOL_FAN', self.rtc.ON)
+            self.rtc.save_to_json()
             time.sleep(60)  # Adjust this value as per your requirement
 
-if __name__ == "__main__":
-    temp_controller = TemperatureController()
-    temp_controller.control_temperature()
+
+
