656a6ef2a50e15792b631628abe6f63bfef0b8d8
Ogalababa <delay7.su@gmail.com>
2023-10-07 21:55:13 +0200

    修复bug

--- ToDB.py
+++ ToDB.py
@@ -16,8 +16,8 @@ class ConnectToDB:
         self.db_name = db_name
 
     def save_to_sql(self, data_dict):
-        df = pd.DataFrame({key: [value] for key, value in data_dict.items()})
-        df.to_sql(self.db_name, self.conn, index=True, if_exists="append")
+        data_df = pd.DataFrame({key: [value] for key, value in data_dict.items()})
+        data_df.to_sql(self.db_name, self.conn, index=True, if_exists="append")
 
     def read_from_sql(self):
         pd.read_sql_table(self.db_name, self.conn)
