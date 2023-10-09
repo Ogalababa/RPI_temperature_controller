d0675b234d45a27fdaa79ec02962495803794b70
Ogalababa <delay7.su@gmail.com>
2023-10-09 14:45:58 +0200

    修复datetime bug

--- TC.py
+++ TC.py
@@ -37,7 +37,7 @@ class TC:
 
     def get_current_hour(self):
         self.datetime = datetime.now().hour
-        return self.datetime.now().hour
+        return datetime.now().hour
 
     def update_uv_equipment(self):
         if 10 <= self.get_current_hour() < 16:
