2c725e1ae51032b15c9b41278ab432a33b5fff52
Ogalababa <delay7.su@gmail.com>
2023-10-10 18:05:25 +0200

    增加streamlit

--- dashboard.py
+++ dashboard.py
@@ -9,13 +9,12 @@ from __init__ import *
 
 db = ConnectToDB("Status", os.path.join(current_dir, 'data'))
 
-# # while True:
-# df = db.read_from_sql()
-# # fig = px.line(df, x="时间", y=['温度', '湿度'],
-# #               hover_data=['加温风扇', '降温风扇', '陶瓷灯', 'UV 灯', '日光灯'])
-# # st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-# # time.sleep(60)
-# st.tabs(df)
-df = db.read_from_sql()
-print(df)
+while True:
+    df = db.read_from_sql()
+    fig = px.line(df, x="时间", y=['温度', '湿度'],
+                  hover_data=['加温风扇', '降温风扇', '陶瓷灯', 'UV 灯', '日光灯'])
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+    time.sleep(60)
+
+
 
