1763bc8debac02c2467ea0737dddb22dafe3b345
Ogalababa <delay7.su@gmail.com>
2023-10-11 09:41:34 +0200

    更新streamlit layout

--- dashboard.py
+++ dashboard.py
@@ -22,6 +22,18 @@ placeholder = st.empty()
 
 # 添加一个滑块来让用户选择刷新间隔
 refresh_interval = st.sidebar.slider('选择刷新间隔（秒）', min_value=1, max_value=60, value=10)
+col1, col2, col3 = st.columns(3)
+metric1 = col1.empty()
+metric2 = col2.empty()
+metric3 = col3.empty()
+# 创建另外四列
+col4, col5, col6, col7, col8 = st.columns(5)
+# 在每列中显示度量值
+metric4 = col4.empty()
+metric5 = col5.empty()
+metric6 = col6.empty()
+metric7 = col7.empty()
+metric8 = col8.empty()
 
 while True:
     # 从数据库中读取数据
@@ -37,23 +49,11 @@ while True:
     # 获取最后一行数据
     last_row = df.tail(1)
     # 创建三列
-    col1, col2, col3 = st.columns(3)
-    metric1 = col1.empty()
-    metric2 = col2.empty()
-    metric3 = col3.empty()
+
     # 在每列中显示度量值
     metric1.metric("温度", f"{last_row['温度'].values[0]:.2f} °C")
     metric2.metric("湿度", f"{last_row['湿度'].values[0]:.2f} %")
     metric3.metric("最后更新", f"{last_row['时间'].values[0]} ")
-
-    # 创建另外四列
-    col4, col5, col6, col7, col8 = st.columns(5)
-    # 在每列中显示度量值
-    metric4 = col4.empty()
-    metric5 = col5.empty()
-    metric6 = col6.empty()
-    metric7 = col7.empty()
-    metric8 = col8.empty()
     metric4.metric("加温风扇", last_row['加温风扇'].values[0])
     metric5.metric("降温风扇", last_row['降温风扇'].values[0])
     metric6.metric("陶瓷灯", last_row['陶瓷灯'].values[0])
