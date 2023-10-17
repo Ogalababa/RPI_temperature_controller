87a8decabfb0eda791ac3f334ab996fee2be3dc2
Ogalababa <delay7.su@gmail.com>
2023-10-17 22:57:16 +0200

    更改日间目标温度

--- run/schedule.py
+++ run/schedule.py
@@ -10,7 +10,7 @@ class Schedule:
     def __init__(self):
         self.rtc = RTC()
         self.target_day = 28
-        self.target_night = 30
+        self.target_night = 29
         self.target_temp = 27
         self.temp_range = 2
         self.is_night = True
