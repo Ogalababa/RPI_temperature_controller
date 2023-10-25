e45bd5a23d81bf0929a813768a914e8e538495fc
Ogalababa <delay7.su@gmail.com>
2023-10-25 09:26:54 +0200

    修复数据测量错误

--- run/data_correction.py
+++ run/data_correction.py
@@ -2,26 +2,38 @@
 # coding:utf-8
 # sys
 import pandas as pd
-from __init__ import *
+from scipy.stats import zscore
 import os
 from sqlalchemy import create_engine
 
-
-conn = create_engine(f'sqlite:///{os.path.join("/", "home", "jiawei", "RPI_temperature_controller", "data", "Status.db")}').connect()
+conn = create_engine(
+    f'sqlite:///{os.path.join("/", "home", "jiawei", "RPI_temperature_controller", "data", "Status.db")}').connect()
 df = pd.read_sql_table("Status", conn)
-corrected_df = df.copy()
+# 1. 加载数据
+data = df.copy()
+data['时间'] = pd.to_datetime(data['时间'])
 
-# 遍历每个测量列
-for column in ['温度', '湿度']:
-    # 计算每个点与其周围点的平均值的差异
-    diff = df[column] - df[column].rolling(window=3, center=True).mean()
-    # 如果差异大于某个阈值（例如，固定阈值5或基于标准偏差的阈值），则修正该点
-    # threshold = 3 * diff.std()
-    threshold = 4
-    outliers = diff.abs() > threshold
-    # 用前后点的平均值替换异常值
-    corrected_df.loc[outliers, column] = (df[column].shift()[outliers] + df[column].shift(-1)[outliers]) / 2
+# 2. 使用z分数方法检测异常值
+data['温度_zscore'] = zscore(data['温度'])
+data['湿度_zscore'] = zscore(data['湿度'])
 
-corrected_df.to_sql("Status", conn, if_exists="replace")
+data['温度_anomaly'] = abs(data['温度_zscore']) > 3
+data['湿度_anomaly'] = abs(data['湿度_zscore']) > 3
+
+anomalies = data[(data['温度_anomaly']) | (data['湿度_anomaly'])]
+
+
+# 3. 使用前后数据的平均值替换异常值
+def replace_with_neighbors_average(series, anomalies_index):
+    for idx in anomalies_index:
+        if idx > 0 and idx < len(series) - 1:
+            series[idx] = (series[idx - 1] + series[idx + 1]) / 2
+    return series
+
+
+data['温度'] = replace_with_neighbors_average(data['温度'], anomalies[anomalies['温度_anomaly']].index)
+data['湿度'] = replace_with_neighbors_average(data['湿度'], anomalies[anomalies['湿度_anomaly']].index)
+
+data.to_sql("Status", conn, if_exists="replace")
 
 conn.close()
