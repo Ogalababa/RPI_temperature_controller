11a22e51bbec62762df34b741741299627435413
Ogalababa <delay7.su@gmail.com>
2023-11-17 23:35:50 +0100

    电子狗添加logger

--- run/dogger.py
+++ run/dogger.py
@@ -6,33 +6,42 @@ from sqlalchemy import create_engine
 from datetime import datetime, timedelta
 import time
 import subprocess
+import logging
+
+# 配置日志记录
+logging.basicConfig(filename='/home/jiawei/RPI_temperature_controller/dogger.log', level=logging.INFO,
+                    format='%(asctime)s %(levelname)s:%(message)s')
 
 
 def restart_raspberry_pi():
-    # 使用 subprocess 来重启树莓派
-    subprocess.run(["sudo", "reboot"])
+    try:
+        logging.info("Restarting Raspberry Pi due to time discrepancy in database records")
+        subprocess.run(["sudo", "reboot"])
+    except Exception as e:
+        logging.error(f"Error occurred while restarting Raspberry Pi: {e}")
 
 
 def check_last_record_time():
-    conn = create_engine(
-        f'sqlite:///{os.path.join("/", "home", "jiawei", "RPI_temperature_controller", "data", "Status.db")}').connect()
+    try:
+        conn = create_engine(
+            f'sqlite:///{os.path.join("/", "home", "jiawei", "RPI_temperature_controller", "data", "Status.db")}').connect()
 
-    # 读取最后一条记录
-    last_record = pd.read_sql_query("SELECT * FROM Status ORDER BY 时间 DESC LIMIT 1", conn)
-    conn.close()
+        last_record = pd.read_sql_query("SELECT * FROM Status ORDER BY 时间 DESC LIMIT 1", conn)
+        conn.close()
 
-    # 检查记录的时间
-    if not last_record.empty:
-        last_record_time = pd.to_datetime(last_record['时间'].iloc[0])
-        current_time = datetime.now()
+        if not last_record.empty:
+            last_record_time = pd.to_datetime(last_record['时间'].iloc[0])
+            current_time = datetime.now()
 
-        # 检查时间差
-        if current_time - last_record_time > timedelta(minutes=10):
-            return True
+            if current_time - last_record_time > timedelta(minutes=10):
+                return True
+    except Exception as e:
+        logging.error(f"Error while checking last record in database: {e}")
     return False
 
 
 def main():
+    logging.info("Database monitoring script started")
     while True:
         if check_last_record_time():
             restart_raspberry_pi()
