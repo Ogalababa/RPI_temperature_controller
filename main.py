577f788534fe55103382695a189f95dac69a62ad
Ogalababa <delay7.su@gmail.com>
2024-07-16 12:38:02 +0200

    移除steamlit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # ！/usr/bin/python3
 # coding:utf-8
 # sys
-from run.schedule import Schedule
+from run.noDB_Schedule import Schedule
 
-temp_controller = Schedule()
+temp_controller = Schedule(day_time=10, uv_start_time=16, night_time=22, day_temp=30,  night_temp=28, target_temp=29)
 temp_controller.controller()
