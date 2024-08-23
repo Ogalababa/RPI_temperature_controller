fe214dab989d1ff661ad247fa0536803ae4e462e
Ogalababa <delay7.su@gmail.com>
2024-08-23 11:40:26 +0200

    优化蓝牙读取温度

--- run/mi_temp.py
+++ run/mi_temp.py
@@ -103,5 +103,9 @@ if __name__ == "__main__":
     else:
         logger.info("Failed to retrieve temperature and humidity data.")
 
-    temperature2, humidity = test_mi_device("58:2D:34:30:53:58")
+    temperature2, humidity2 = test_mi_device("58:2D:34:30:53:58")
+    if temperature2 and humidity2:
+        logger.info(f"Final Temperature: {temperature2}°C, Final Humidity: {humidity2}%")
+    else:
+        logger.info("Failed to retrieve temperature and humidity data.")
 
